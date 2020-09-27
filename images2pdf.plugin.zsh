66682aee6090613bdbfb373413763614bbb5d64b
Felix Stupp <felix.stupp@outlook.com>
2020-09-27 22:38:15 +0200

    Replaced exit to return to not exit shell on Usage print

--- images2pdf.plugin.zsh
+++ images2pdf.plugin.zsh
@@ -4,7 +4,7 @@ function images2pdf() {
   if [ -z "$3" ]; then
     echo "Usage: images2pdf <lang> <output-pdf> <input-file> …" >&2;
     echo "  lang: languages separated by + characterized by 3 characters, example: deu+eng" >&2;
-    exit 2;
+    return 2;
   fi
   convert "${@:3}" pdf:- | ocrmypdf -l "$1" - "$2";
 }
